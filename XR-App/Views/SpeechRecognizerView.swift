a13cca58d6a2f4dfdcafba6eaa09614570baa9da
Lisa Kohls <kohls_lisa@web.de>
2025-03-29 17:24:29 +0100

    view

---
+++ XR-App/Views/SpeechRecognizerView.swift
@@ -0,0 +1,61 @@
+//
+//  SpeechRecognizerView.swift
+//  XR-App
+//
+//  Created by Lisa Kohls on 29.03.25.
+//
+
+import SwiftUI
+import Speech
+
+struct SpeechRecognizerView: View {
+    
+    @Binding var recognizedText: String
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var isListening = false
+    private let speechRecognizer = SpeechRecognizer()
+    
+    var body: some View {
+           VStack {
+               Text("Say the name of the object")
+                   .font(.title3)
+
+               Text(recognizedText.isEmpty ? "Listening..." : recognizedText)
+                   .font(.headline)
+               
+               HStack {
+                   Button(action: {
+                       if isListening {
+                           speechRecognizer.stopRecognition()
+                           isListening = false
+                       } else {
+                           speechRecognizer.configureAudioSession()
+                           
+                           speechRecognizer.onResult = { textResult in
+                               print("Erkannt: \(textResult)")
+                               recognizedText = textResult
+                           }
+                           
+                           speechRecognizer.startRecognition()
+                           isListening = true
+                       }
+                   }) {
+                       Text(isListening ? "Stop" : "Start Listening")
+                           .clipShape(Capsule())
+                   }
+                   
+                   Button("Close") {
+                       dismiss()
+                   }
+                   .background(Color.gray.opacity(0.2))
+                   .clipShape(Capsule())
+               }
+           }
+           .onDisappear {
+               speechRecognizer.stopRecognition()
+           }
+           .padding()
+       }
+   }
+
